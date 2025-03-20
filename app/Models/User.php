9e9f0e234ab5a8aaa378c7655a238dd5457754dc
ali-rostom1 <ali.rostom220@gmail.com>
2025-03-20 14:24:36 +0000

    modify user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function plants()
+    {
+        return $this->hasMany(Plant::class);
+    }
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
