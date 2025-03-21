93739c7f783da0f5238c90c7e94a7831a98576a9
ali-rostom1 <ali.rostom220@gmail.com>
2025-03-21 11:19:51 +0000

    add api rate limiting of 60 rq per min

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function(){
 
     Route::post('/register',[AuthController::class,'register']);
-
-
+    Route::apiResources(['plants','orders','images'],   );
 });
